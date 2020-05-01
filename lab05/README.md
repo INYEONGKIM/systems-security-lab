e4292ad54ff9ee9c88671d365355936f0edca41d
INYEONGKIM <hacerrumbo@gmail.com>
2020-05-02 02:03:36 +0900

    lab05 : fin

--- lab05/README.md
+++ lab05/README.md
@@ -2,9 +2,29 @@
 - build
 ```
 $ gcc -m32 -z execstack -fno-stack-protector -g task2_stack.c -o task2_stack
+$ gcc -o task2_exploit -m32 task2_exploit.c
 ```
 
 - execute
 ```
-$ 
+$ sudo chown root task2_stack
+$ sudo chmod 4755 task2_stack
+$ ./task2_exploit
+$ ./task2_stack
+$ # id
+```
+
+## TASK 3
+- build
+```
+$ gcc -o task3_exploit -m32 task3_exploit.c
+```
+
+- execute
+```
+$ sudo chown root task3_stack
+$ sudo chmod 4755 task3_stack
+$ ./task3_exploit
+$ ./task2_stack
+$ $ id
 ```
---
+++ lab05/t.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef BUF_SIZE
+#define BUF_SIZE 12
+#endif
+
+int bof(char *str){
+	char *buf = (char *) malloc(sizeof(str));
+	int size;
+	printf("%u\n", size);
+
+	strcpy(buf, str);
+
+	return 1;
+}
+
+int main(int agrc, char **argv){
+	char str[517];
+	FILE *badfile;
+	char dummy[BUF_SIZE];
+	memset(dummy, 0, BUF_SIZE);
+
+	badfile = fopen("badfile_2016015878", "r");
+	fread(str, sizeof(char), 517, badfile);
+	bof(str);
+	printf("Returned Properly\n");
+
+	return 1;
+}
