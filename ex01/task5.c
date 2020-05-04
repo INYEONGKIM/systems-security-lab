bcb15aea8b5fc5f7e6a840c9e8012ca4a0149a6c
INYEONGKIM <hacerrumbo@gmail.com>
2020-05-04 17:00:17 +0900

    lab05 : fin

--- ex01/task5.c
+++ ex01/task5.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define KEYSIZE 16
+//#define KEYSIZE 16
+#define KEYSIZE 32
 
 void main(){
 	unsigned char *key = (unsigned char *) malloc(sizeof(unsigned char)*KEYSIZE);
 	FILE* random = fopen("/dev/urandom", "r");
 
 	fread(key, sizeof(unsigned char)*KEYSIZE, 1, random);
-	printf("%.2x\n", *key);
+	
+	for(int i = 0; i < KEYSIZE; i++){
+		printf("%.2x ", *(key + i));
+	}
+	printf("\n");
 	fclose(random);
 }
